1c2188ef5698e3fa77631be351ab37cecf5c7bb8
Akiya Xiao <akiya.xiao@icloud.com>
2020-12-18 15:24:35 +0800

    Get ship preview info !🎇

--- src/endeavor/items.py
+++ src/endeavor/items.py
@@ -10,3 +10,12 @@ class DmozItem(scrapy.Item):
     title = scrapy.Field()
     link = scrapy.Field()
     desc = scrapy.Field()
+
+
+class ShipItem(scrapy.Item):
+    Name = scrapy.Field(serializer=str)
+    Manufacturer = scrapy.Field(serializer=str)
+    Size = scrapy.Field(serializer=str)
+    Role = scrapy.Field(serializer=list)
+    FlightReady = scrapy.Field(serializer=bool)
+    PledgePrice = scrapy.Field(serializer=int)
--- src/endeavor/spiders/ship_spider.py
+++ src/endeavor/spiders/ship_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import logging as log
+from endeavor.items import ShipItem
 
 
 class ShipSpider(scrapy.Spider):
@@ -13,10 +14,34 @@ class ShipSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        ship_names = response.xpath(
-            '//td/a/text()').extract()
-        ship_names = list(
-            filter(lambda x: x != 'Squadron 42' and '(' not in x, ship_names))
-        log.info(f'Total ship count: {len(ship_names)}')
-        for name in ship_names:
-            log.info(name)
+        ships = response.xpath('//table//tr')
+        count = 0
+        for ship in ships[1:]:
+            item = ShipItem()
+            # name
+            item['Name'] = ship.xpath(
+                'td[1]//text()').extract_first()
+            # manufacturer
+            item['Manufacturer'] = ship.xpath(
+                'td[2]//text()').extract_first().replace('\n', '')
+            # role
+            item['Role'] = ship.xpath(
+                'td[3]//text()').extract_first().replace(
+                    '\n', '').replace(' ', '').split('/')
+            # size
+            item['Size'] = ship.xpath(
+                'td[4]//text()').extract_first().replace('\n', '')
+            # flight ready
+            state = ship.xpath(
+                'td[6]//text()').extract_first().replace('\n', '')
+            item['FlightReady'] = state == 'Flight ready'
+            # price
+            price = ship.xpath(
+                'td[7]//text()').extract_first().replace('\n', '')
+            item['PledgePrice'] = price
+            sc_state = ['Flight ready', 'In concept', 'In production']
+            if state in sc_state and price:
+                count += 1
+                log.info(f'Get ship: {item["Name"]}')
+                yield item
+        log.info(f'Total ship count: {count}.')
